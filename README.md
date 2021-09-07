ff4c3405eb7b8e43938dabaa68837e2143578c14
Jacob Parris <typefortyproductions@gmail.com>
2021-09-07 19:44:57 -0400

    Seems to be working in Totality EXCEPT: Color Current Hour

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # UNC_HW_Week_5_Calender
+
+This was built for my 5th week of homework in UNC Bootcamp.
+
+# User Instructions
+
+To Add A Task:
+- Type Task in Form with "Add Task" Placeholder Text
+- Enter Task you wish to track for the day
+- Click the Save Button to save the task to localstorage
+
+To Delete a Task:
+- Click the Clear button on the individual hour line
+
+Color Functions to show which hours have passed (Grey) which hours are ahead (light Blue) and which are current (Green, but currently not working right)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var date = moment().format("MMMM Do YYYY hh:mm a");
+var cur_int = moment("12:00:00 pm", "HH:mm:ss");//Placeholder "Time"
+//var cur_int = moment();//Actual Time- Comment as needed
 
 $("#currentDay").text(date);
 
@@ -9,7 +11,7 @@ var day_start = "09:00:00"
 
 for (i = 0 ; i < 9; i++){
     //This Loop Creates the daily hours/columns
-    hour = moment(day_start,"HH:mm:ss").add(i,'hours').format("hh a");
+    var hour = moment(day_start,"HH:mm:ss").add(i,'hours').format("hh a");
     var row = $("<div class=row>");
     var sch_form = $("<div class='input-group'>");
     var sch_pre = $("<div class='input-group-prepend'>")
@@ -28,6 +30,24 @@ for (i = 0 ; i < 9; i++){
     del_bttn.attr("id", del_id);
     del_bttn.text("Clear");
     date_col.text(hour);
+    
+    var hour_int = moment(day_start,"hh a").add(i, 'hours')
+    if (hour_int < cur_int) {
+        //This changes the background of the line to grey if the hour has passed
+        date_col.attr("style","background-color: grey");
+        //act_col.attr("style","background-color: grey");
+    } else if (hour_int === cur_int){
+        //This is supposed to change the background to green if it is the current hour
+        console.log("it's the current time" + hour + " " + cur_int);
+        date_col.attr("style","background-color: green");
+        //act_col.attr("style","background-color: green");
+    } else {
+        //This is supposed to change the background to blue if the hour is ahead
+        console.log("It's Early" + hour_int + " " + cur_int);
+        date_col.attr("style","background-color: lightblue");
+        //act_col.attr("style","background-color: lightblue");
+    }
+
     act_col.val(localStorage.getItem(mem_id));
     sch_pre.append(date_col);
     sch_end.append(save_col);
@@ -58,10 +78,3 @@ $(".task-del").click(function(){
     localStorage.removeItem(mem_locale);
     location.reload();
 })
-//If statement to highlight current hour
-
-///If (.hourblock === moment(current time)){
-//    this.attr("style","background-color:red")
-//}
-
-$(".hourblock").attr("style","background-color: red");
